f6a69522b0433cc6778d53cf90158c317d937877
Simon Kalouche <simon.kalouche@gmail.com>
2017-08-06 20:34:48 -0700

    initial commit

---
+++ Retail Shelf Images/code_resources/iCloud_comm.py
@@ -0,0 +1,32 @@
+import ricloud
+import pyicloud
+from pyicloud import PyiCloudService # for sending data to icloud (https://pypi.python.org/pypi/pyicloud)
+from ricloud.api import ricloud
+from ricloud.api import BackupClient 
+
+
+# Resources
+# https://cdn.apple-cloudkit.com/cloudkit-catalog/
+
+
+# ####################### pyicloud ##########################################
+# # enter credentials to access the cloud
+# api = PyiCloudService('simon.kalouche@gmail.com','Applebucks')
+# iphone = api.devices['/No8NCNvg7czzHptNiecXBZzNoZZotRZNEri7BmvOytCQY75uWW2+OHYVNSUzmWV']
+# # api = PyiCloudService('adam.selevan5@gmail.com', 'DiscoLabs1')
+# # iphone = api.devices['BBuqlCRus9qa6+YmLWEjPHz6jMYyM5t7ToI0eu+IoZeUMPKD0Ag0euHYVNSUzmWV']
+
+# # send message to iphone
+# iphone.display_message('product out of stock')
+
+
+
+# ###########################################################################
+
+
+
+
+
+# ########################## ricloud #########################################
+# api = RiCloud()
+# api.loginn('adam.selevan5@gmail.com','DiscoLabs1')
